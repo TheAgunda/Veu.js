0124a7f91422c30371388a814cfcb6b36fb7c230
badolakiran96 <kiran.kumar@hachiweb.com>
2021-06-25 23:18:22 +0530

    Make Conroller and register in App

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <img alt="Vue logo" src="../assets/logo.png" height="40" width="40" />
+    <h5>Welcome to DevelopersBlog</h5>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+};
+</script>
+<style scoped>
+.header {
+  margin: 0px;
+  background-color:azure;
+  height: 50px;
+  width: 100%;
+}
+body{
+  padding: 0px;
+  margin: 0px;
+}
+</style>
+
